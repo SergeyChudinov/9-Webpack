5259b119b77bff8230ed1804f2f1981f11adc2f1
Sergey Chudinov <sergeychudinov1986@gmail.com>
2023-07-06 23:08:19 +0500

    Дополнительные настройки

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import Post from './Post'
+import Post from '@models/Post'
+import json from './assets/json'
+import xml from './assets/data.xml'
+import csv from './assets/data.csv'
+import WebpackLogo from '@/assets/webpack-logo.png'
+import './styles/styles.css'
 
-const post = new Post('Webpack Post Title')
+const post = new Post('Webpack Post Title', WebpackLogo)
 console.log('Post to String', post.toString())
+
+console.log('JSON', json)
+console.log('XML:', xml)
+console.log('CSV:', csv)
 
 
